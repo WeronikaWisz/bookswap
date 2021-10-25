1911f228ba08c75abff9220596e92baf4c9aa35c
Weronika Wisz <wisz.weronika@gmail.com>
2021-10-25 23:48:29 +0200

    swap status correction

--- bookswap-app/src/main/java/com/bookswap/bookswapapp/enums/ESwapStatus.java
+++ bookswap-app/src/main/java/com/bookswap/bookswapapp/enums/ESwapStatus.java
@@ -2,5 +2,10 @@ package com.bookswap.bookswapapp.enums;
 
 public enum ESwapStatus {
     IN_PROGRESS,
+    BOOK_1_CONFIRMED,
+    BOOK_2_CONFIRMED,
+    BOTH_CONFIRMED,
+    BOOK_1_RETURNED,
+    BOOK_2_RETURNED,
     COMPLETED
 }
